a44ae4591577177cf3a9156511ccc41ab42fc3a8
catodalys <139085719+catodalys@users.noreply.github.com>
2023-07-16 22:58:45 -0600

    Contact Us page added

--- slidesscript.js
+++ slidesscript.js
@@ -1,1 +1,19 @@
-// Add your JavaScript code heredocument.addEventListener("DOMContentLoaded", function() {  var slides = document.getElementsByClassName("slide");  var slideIndex = 0;  function showSlide() {    for (var i = 0; i < slides.length; i++) {      slides[i].style.display = "none";    }    slideIndex++;    if (slideIndex > slides.length) {      slideIndex = 1;    }    slides[slideIndex - 1].style.display = "block";    setTimeout(showSlide, 3000); // Change slide every 3 seconds  }  showSlide();});
+// Add your JavaScript code here
+document.addEventListener("DOMContentLoaded", function() {
+  var slides = document.getElementsByClassName("slide");
+  var slideIndex = 0;
+
+  function showSlide() {
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 3000); // Change slide every 3 seconds
+  }
+
+  showSlide();
+});
